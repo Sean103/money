fcc814d7d95395a139b241699d7deecdaa6b93f1
xiaoshengkang <898987943@qq.com>
2021-02-01 22:27:54 +0800

    完成css，提取了几个通用组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,11 +1,19 @@
 <template>
-    <Layout>
-       <div>
-           <Icon name="left"></Icon>
-           <span>编辑标签</span>
-       </div>
-       <Notes fieldName="标签名" placeholder="请输入标签名"></Notes>
-    </Layout>
+  <Layout>
+    <div class="navBar">
+      <Icon class="leftIcon" name="left" @click="goBack"/>
+      <span class="title">编辑标签</span>
+      <span class="rightIcon"></span>
+    </div>
+    <div class="form-wrapper">
+      <Notes :value="tag.name"
+                @update:value="update"
+                field-name="标签名" placeholder="请输入标签名"/>
+    </div>
+    <div class="button-wrapper">
+      <Button @click="remove">删除标签</Button>
+    </div>
+  </Layout>
 </template>
 
 <script>
@@ -38,5 +46,33 @@ import Notes from '@/components/Money/Notes';
 </script>
 
 <style lang="scss" scoped>
+  .navBar {
+    text-align: center;
+    font-size: 16px;
+    padding: 12px 16px;
+    background: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    > .title {
+    }
+    > .leftIcon {
+      width: 24px;
+      height: 24px;
+    }
+    > .rightIcon {
+      width: 24px;
+      height: 24px;
+    }
+  }
+  .form-wrapper {
+    background: white;
+    margin-top: 8px;
+  }
+  .button-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
 
 </style>
