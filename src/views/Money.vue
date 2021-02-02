700dba81f2e753bf1ee7d0d3a68cb2130f60df54
xiaoshengkang <898987943@qq.com>
2021-02-02 11:11:53 +0800

    将create封装到recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -54,16 +54,14 @@
         }
 
         saveRecord(){
-            const record2: RecordItem = recordListModel .clone(this.record);
-            record2.createAt = new Date();
-            this.recordList.push(record2);
+            recordListModel.create(this.record);
 
         }
 
         @Watch('recordList')
 
         onRecordListChannge() {
-           recordListModel .save(this.recordList);
+           recordListModel .save();
         }
        
         
